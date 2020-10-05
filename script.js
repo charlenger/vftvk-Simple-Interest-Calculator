e071e82d426cb486484487bad416bb5f3e81e888
Carlo Alberto Hung <carlo.ah@gmail.com>
2020-10-06 00:20:34 +0200

    IBM Course quick exercise

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	principalElement = document.getElementById("principal");
+    principal = principalElement.value;
+	if(isNaN(principal) || principal.length == 0) {
+		alert("Please insert a positive number");
+		principalElement.focus();
+		principalElement.classList.add("redBorder");
+	} else {
+		principalElement.classList.remove("redBorder");
+		rate = document.getElementById("rate").value;
+		years = document.getElementById("years").value;
+		interest = principal * years * rate / 100;
+		currentDate = new Date();
+		currentDate.setFullYear(currentDate.getFullYear() + parseInt(years));
+		futureYear = currentDate.getFullYear();
+
+		document.getElementById("result").innerHTML = "If you deposit " + principal
+													+ "<br/> at an interest rate of " + rate + "%"
+													+ "<br/> You will receive an amount of " + interest
+													+ "<br/> in the year " + futureYear
+	}
+}
+
+function updateRate() {
+	rate = document.getElementById("rate").value;
+	document.getElementById("rateValue").innerHTML = rate + "%";
 }
         
